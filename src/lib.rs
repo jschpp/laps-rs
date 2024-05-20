37aa9441a60588a58f160dbf7225dac6667c2eb4
Johannes Schöpp <darkdiscover@gmail.com>
2024-05-20 14:13:20 +0200

    feat: :building_construction: Use reexports to only export the needed public functions
    This will give a greater bit of control over what to expose to the public API
    
    Some APIs are no longer public

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 mod decryption;
 mod error;
 mod helpers;
-pub mod ldap;
-pub mod settings;
+mod ldap;
+mod settings;
+
+pub use decryption::decrypt_password_blob_ng;
+pub use error::*;
+pub use ldap::{retrieve_laps_info, LdapProtocol, MsLapsPassword};
+pub use ldap3::Scope;
+pub use settings::*;
