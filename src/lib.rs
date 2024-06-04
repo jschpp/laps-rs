e1d87999712211c1c77114dc8046eb4782f1481b
Johannes Schöpp <darkdiscover@gmail.com>
2024-06-04 21:09:44 +0200

    feat: :sparkles: Added higher level helper to retrieve laps information

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,34 @@ mod helpers;
 mod ldap;
 mod settings;
 
-pub use decryption::decrypt_password_blob_ng;
 pub use error::*;
-pub use ldap::{retrieve_laps_info, LdapProtocol, MsLapsPassword};
+pub use ldap::{
+    lookup_laps_info, lookup_laps_info_async, process_ldap_search_result, LdapProtocol,
+    MsLapsPassword,
+};
 pub use ldap3::Scope;
 pub use settings::*;
+
+pub fn get_laps_info(
+    computer_name: &str,
+    settings: AdSettings,
+) -> Result<MsLapsPassword, LapsError> {
+    // construct ldap connection uri
+    let prot: &str = settings.protocol.into();
+    let con_str = format!("{}://{}:{}", prot, settings.server, settings.port);
+
+    // bind
+    let mut con: ldap3::LdapConn =
+        ldap3::LdapConn::new(&con_str).map_err(|e| LapsError::LdapError(e.to_string()))?;
+    con.sasl_gssapi_bind(&settings.server)
+        .map_err(|e| LapsError::LdapError(e.to_string()))?;
+    let result = lookup_laps_info(
+        computer_name,
+        &mut con,
+        &settings.search_base,
+        settings.scope,
+    );
+    con.unbind()
+        .map_err(|e| LapsError::LdapError(e.to_string()))?;
+    process_ldap_search_result(result)
+}
