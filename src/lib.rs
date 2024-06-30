86e98568d71ff2f55016e45e297805ea188264d3
Johannes Schöpp <darkdiscover@gmail.com>
2024-06-30 15:43:54 +0200

    refactor: :fire: Remove now superfluous `get_laps_info()`
    With the new API design this function will no longer be useful

--- src/lib.rs
+++ src/lib.rs
@@ -77,30 +77,3 @@ pub use error::*;
 pub use ldap::{lookup_laps_info, lookup_laps_info_async, process_ldap_search_result};
 pub use ldap3::Scope;
 pub use types::*;
-
-pub fn get_laps_info(
-    computer_name: &str,
-    settings: AdSettings,
-) -> Result<MsLapsPassword, LapsError> {
-    // construct ldap connection uri
-    let con_str = settings.get_connection_uri();
-
-    // bind
-    let mut con: ldap3::LdapConn = ldap3::LdapConn::new(&con_str)?;
-    con.sasl_gssapi_bind(&settings.server_fqdn)?;
-    let mut con = AdConnection { ldap: con };
-
-    // lookup
-    let result = lookup_laps_info(
-        computer_name,
-        &mut con,
-        &settings.search_base,
-        settings.scope,
-    );
-
-    // unbind
-    con.ldap.unbind()?;
-
-    // process and decrypt (if necessary)
-    process_ldap_search_result(result)
-}
