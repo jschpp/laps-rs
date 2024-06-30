77d47b8960115311d516e8b3ca36dd97750db805
Johannes Schöpp <darkdiscover@gmail.com>
2024-06-30 16:46:32 +0200

    docs: :memo: More documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,27 @@
 #![cfg(windows)]
 
-//! # LAPS Password retrieval
-//!
-//! This crate enables the retrieval of a [LAPS](https://learn.microsoft.com/en-us/windows-server/identity/laps/laps-overview) passwords.
+//! Library for the retrieval of [LAPS](https://learn.microsoft.com/en-us/windows-server/identity/laps/laps-overview) passwords
 //!
 //! Central for that are the two structs [`AdConnection`] and [`AdConnectionAsync`] which hold a connection to the Active Directory and implement `try_search()`
 //!
-//! ## Example
+//! # Usage
+//!
+//! In `Cargo.toml`:
+//! ```toml
+//! [dependencies.laps_rs]
+//! version = 0.1.0
+//! ```
+//!
+//! # Example
 //!
 //! Both examples perform a search for `"computername"`
 //!
-//! ### Synchronous search
+//! ## Synchronous search
 //! ```rust,no_run
+//! # use std::error::Error;
 //! use laps_rs::{AdSettings, AdConnection, LdapProtocol, Scope};
 //!
+//! # fn main() -> Result<(), Box<dyn Error>> {
 //!let settings = AdSettings::new(
 //!    "dc.test.internal",
 //!    636,
@@ -21,45 +29,43 @@
 //!    "OU=path,OU=to,OU=computers,DC=test,DC=internal",
 //!    Scope::Subtree,
 //!);
-//!let mut con: AdConnection = settings.connect().expect("working domain controller");
-//!let password = con
-//!    .try_search("computername", &settings)
-//!    .expect("computer exists and has a LAPS Password");
+//!let mut con: AdConnection = settings.connect()?;
+//!let password = con.try_search("computername", &settings)?;
 //!println!("{password:?}");
+//! # Ok(())
+//! # }
 //! ```
 //!
 //! See also [`AdConnection::try_search()`]
 //!
-//! ### Asynchronous search
-// ignore here because correctly importing tokio in doc comments annoys me
-// FIXME: correctly import tokio here to change this to no_run
-//! ```rust,ignore
+//! ## Asynchronous search
+//!
+//! ```rust,no_run
+//! # use std::error::Error;
 //! use laps_rs::{AdSettings, AdConnectionAsync, LdapProtocol, Scope};
 //!
-//! #[tokio::main]
-//! async fn main() {
-//!     let settings = AdSettings::new(
-//!         "dc.test.internal",
-//!         636,
-//!         LdapProtocol::Secure,
-//!         "OU=path,OU=to,OU=computers,DC=test,DC=internal",
-//!         Scope::Subtree,
-//!     );
-//!     let mut con: AdConnectionAsync = settings
-//!         .connect_async()
-//!         .await
-//!         .expect("working domain controller");
-//!     let password = con
-//!         .try_search("computername", &settings)
-//!         .await
-//!         .expect("computer exists and has a LAPS Password");
-//!     println!("{password:?}");
-//! }
+//!# async fn example() -> Result<(), Box<dyn Error>> {
+//!let settings = AdSettings::new(
+//!    "dc.test.internal",
+//!    636,
+//!    LdapProtocol::Secure,
+//!    "OU=path,OU=to,OU=computers,DC=test,DC=internal",
+//!    Scope::Subtree,
+//!);
+//!let mut con: AdConnectionAsync = settings
+//!    .connect_async()
+//!    .await?;
+//!let password = con
+//!    .try_search("computername", &settings)
+//!    .await?;
+//!println!("{password:?}");
+//! # Ok(())
+//! # }
 //! ```
 //!
 //! See also [`AdConnectionAsync::try_search()`]
 //!
-//! ## Quirks
+//! # Quirks
 //!
 //! Since it can be the case that both encrypted and unencrypted LAPS data exists for the same
 //! computer [`process_ldap_search_result()`] will prefer the encrypted information in case of
